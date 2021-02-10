de7296620db92a7ee4df3f2f74222ad25eedad52
Ewgeniy <efk.dev@gmail.com>
2021-02-10 16:34:23 +0700

    first commit

---
+++ server/src/__tests__/acceptance/test-helper.ts
@@ -0,0 +1,32 @@
+import {RealEstateApp} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const restConfig = givenHttpServerConfig({
+    // Customize the server configuration here.
+    // Empty values (undefined, '') will be ignored by the helper.
+    //
+    // host: process.env.HOST,
+    // port: +process.env.PORT,
+  });
+
+  const app = new RealEstateApp({
+    rest: restConfig,
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: RealEstateApp;
+  client: Client;
+}
